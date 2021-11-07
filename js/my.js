738cd8efd70f884d901110ec0e452531bf13b946
Luke Shafer <anahitacreates@gmail.com>
2021-11-07 00:02:29 -0400

    Initial commit

---
+++ js/my.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // Load modular navbar
+    $("#my-navbar").load("./html/nav.html",  // Navbar URL
+        function() { // insert any functions that use the Navbar below
+            $(".navbar-burger").click(function() {
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                $(".navbar-burger").toggleClass("is-active");
+                $(".navbar-menu").toggleClass("is-active");
+            });
+        });
+
+});
